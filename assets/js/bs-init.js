e3ab48ccf74e3d945f04b6fda66e2c53b19d96e5
Ankit Datta <ankit.d@surya-soft.com>
2022-07-20 15:21:43 +0530

    first commit

---
+++ assets/js/bs-init.js
@@ -0,0 +1,24 @@
+
+if (window.innerWidth < 768) {
+	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
+		elem.classList.remove('animated');
+		elem.removeAttribute('data-bss-hover-animate');
+		elem.removeAttribute('data-aos');
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	AOS.init();
+
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	  return new bootstrap.Tooltip(tooltipTriggerEl);
+	})
+
+	var products = document.querySelectorAll('[data-bss-dynamic-product]');
+
+	for (var product of products) {
+		var param = product.dataset.bssDynamicProductParam;
+		product.dataset.reflowProduct = new URL(location.href).searchParams.get(param)
+	}
+}, false);
